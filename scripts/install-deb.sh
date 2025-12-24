b4d9cb90e86de4df97d3cf1909bf40c352cfbf9c
Giovanni <giovannirafanan609@gmail.com>
2025-12-24 22:29:57 +0800

    fixed install script for debian based systems

--- scripts/install-deb.sh
+++ scripts/install-deb.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 
-URL="https://github.com/Vani1-2/gnome-meme-editor/releases/latest/download/"
-FILE="memerist-amd64.deb"
-
+REPO="Vani1-2/gnome-meme-editor"
 
 if ! command -v apt &> /dev/null; then
     echo "Error: This script requires 'apt' (Debian/Ubuntu)."
     exit 1
 fi
 
+echo "Finding latest release..."
 
-echo "Downloading memerist..."
+
+DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | grep -o "https://.*\.deb" | head -n 1)
+
+if [ -z "$DOWNLOAD_URL" ]; then
+    echo "Error: Could not find a .deb file in the latest release."
+    exit 1
+fi
+
+FILE=$(basename "$DOWNLOAD_URL")
+
+echo "Downloading $FILE..."
+
+
 if command -v curl &> /dev/null; then
-    curl -L -o "$FILE" "$URL"
+    curl -L -o "$FILE" "$DOWNLOAD_URL"
 elif command -v wget &> /dev/null; then
-    wget -O "$FILE" "$URL"
+    wget -O "$FILE" "$DOWNLOAD_URL"
 else
     echo "Error: 'curl' or 'wget' not found, install them."
     exit 1
