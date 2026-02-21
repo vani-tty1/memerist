a752b7cde9ea8c2119acbf6c79a6d1600181d2aa
vani-tty1 <giovannirafanan609@gmail.com>
2026-02-21 20:08:28 +0800

    new link to PKGBUILD

--- scripts/install-archlinux.sh
+++ scripts/install-archlinux.sh
@@ -8,7 +8,7 @@ mkdir -p memerist_build
 cd memerist_build
 
 echo "Downloading build script..."
-curl -L -O https://github.com/Vani1-2/gnome-meme-editor/releases/latest/download/PKGBUILD
+curl -L -O https://github.com/vani-tty1/memerist/releases/latest/download/PKGBUILD
 
 echo "Building and Installing..."
 makepkg -si --noconfirm
