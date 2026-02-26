b25be8dc2db97182cd14400a8a06a87e932de06a
vani-tty1 <giovannirafanan609@gmail.com>
2026-02-26 19:31:37 +0800

    added additional information on about

--- src/about.c
+++ src/about.c
@@ -1,17 +1,25 @@
 #include "config.h"
+#include "gtk/gtk.h"
 #include "about.h"
 
 void
-show_about_dialog (GtkWindow *parent)
-{
+show_about_dialog (GtkWindow *parent) {
   static const char *developers[] = {"vani-tty1", NULL};
+  static const char *designers[] = {"vani-tty1", NULL};
 
   adw_show_about_dialog (GTK_WIDGET (parent),
-                         "application-name", "Meme Editor",
-                         "application-icon", "io.github.vani_tty1.memerist",
-                         "developer-name", "vani-tty1",
-                         "version", PACKAGE_VERSION,
-                         "developers", developers,
-                         "copyright", "©2026 vani-tty1",
-                         NULL);
+                        "application-name", "Memerist",
+                        "application-icon", "io.github.vani_tty1.memerist",
+                        "developer-name", "vani-tty1",
+                        "version", PACKAGE_VERSION,
+                        "developers", developers,
+                        "designers", designers,
+                        "copyright", "©2026 vani-tty1",
+                        "website", "https://github.com/vani-tty1/memerist",
+                        "issue-url", "https://github.com/vani-tty1/memerist/issues",
+                        "support-url", "https://github.com/vani-tty1/memerist/discussions",
+                        "license-type", GTK_LICENSE_GPL_3_0,
+                        "debug-info", "Memerist 0.3.3\nGTK4/libadwaita",
+                        "debug-info-filename", "memerist-debug.txt",
+                        NULL);
 }
