821b2790b805ea347740ef1fa27a6b456b091eba
vani-tty1 <giovannirafanan609@gmail.com>
2026-03-14 22:05:28 +0800

    make the about use the metainfo instead

--- src/about.c
+++ src/about.c
@@ -1,50 +1,40 @@
+#include "adwaita.h"
 #include "config.h"
 #include "gtk/gtk.h"
+#include "gtk/gtkshortcut.h"
+#include <stdio.h>
 #include "about.h"
 
-void show_about_dialog (GtkWindow *parent) {
-  static const char *developers[] = {"vani-tty1", NULL};
-  static const char *designers[] = {"vani-tty1", NULL};
+void show_about_dialog(GtkWindow *parent) {
+    g_autofree char *os_release_content = NULL;
+    g_autoptr(GError) error = NULL;
     
-  GBytes *notes_bytes = g_resources_lookup_data("/io/github/vani_tty1/memerist/release.html", G_RESOURCE_LOOKUP_FLAGS_NONE, NULL);
-  const char *release_notes = notes_bytes ? g_bytes_get_data(notes_bytes, NULL) : "";
-  
-  
-  g_autofree char *os_release_content = NULL;
-  g_autoptr(GError) error = NULL;
+    if (!g_file_get_contents("/etc/os-release", &os_release_content, NULL, &error)) {
+        os_release_content = g_strdup("Could not read /etc/os-release");
+        g_clear_error(&error);
+    }
+      
+    g_autofree char *debug_text = g_strdup_printf (
+        "Memerist %s\n"
+        "GTK version: %d.%d.%d\n"
+        "--- Runtime info ---\n"
+        "%s",
+        PACKAGE_VERSION,
+        gtk_get_major_version (),
+        gtk_get_minor_version (),
+        gtk_get_micro_version (),
+        os_release_content
+    );
+    
+    AdwDialog *dialog = adw_about_dialog_new_from_appdata(
+        "/io/github/vani_tty1/memerist/io.github.vani_tty1.memerist.metainfo.xml", 
+        PACKAGE_VERSION
+    );
+    
+    adw_about_dialog_set_version(ADW_ABOUT_DIALOG(dialog), PACKAGE_VERSION);
 
-  if (!g_file_get_contents("/etc/os-release", &os_release_content, NULL, &error)) {
-    os_release_content = g_strdup("Could not read /etc/os-release");
-    g_clear_error(&error);
-  }
-  
-  g_autofree char *debug_text = g_strdup_printf (
-    "Memerist %s\n"
-    "GTK version: %d.%d.%d\n"
-    "--- System OS Release ---\n"
-    "%s",
-    PACKAGE_VERSION,
-    gtk_get_major_version (),
-    gtk_get_minor_version (),
-    gtk_get_micro_version (),
-    os_release_content
-  );
-
-
-  adw_show_about_dialog (GTK_WIDGET (parent),
-                        "application-name", "Memerist",
-                        "application-icon", "io.github.vani_tty1.memerist",
-                        "developer-name", "vani-tty1",
-                        "version", PACKAGE_VERSION,
-                        "developers", developers,
-                        "designers", designers,
-                        "copyright", "©2025 vani-tty1",
-                        "website", "https://github.com/vani-tty1/memerist",
-                        "issue-url", "https://github.com/vani-tty1/memerist/issues",
-                        "support-url", "https://github.com/vani-tty1/memerist/discussions",
-                        "license-type", GTK_LICENSE_GPL_3_0,
-                        "debug-info", debug_text,
-                        "debug-info-filename", "memerist-debug.txt",
-                        "release-notes", release_notes,
-                        NULL);
+    adw_about_dialog_set_debug_info(ADW_ABOUT_DIALOG(dialog), debug_text);
+    adw_about_dialog_set_debug_info_filename(ADW_ABOUT_DIALOG(dialog), "meme-debug.txt");
+    
+    adw_dialog_present(dialog, GTK_WIDGET(parent));
 }
