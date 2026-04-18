9a82ad18455e8bb5f2e9d184a47021eb07a8d21f
vani-tty1 <giovannirafanan609@gmail.com>
2026-04-18 09:53:06 +0800

    Refactor: Consolidate about dialog into meme-application.c

--- src/about.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <adwaita.h>
-
-G_BEGIN_DECLS
-
-void show_about_dialog (GtkWindow *parent);
-
-G_END_DECLS
--- src/meme-application.c
+++ src/meme-application.c
@@ -18,12 +18,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-#include "about.h"
 #include <glib/gi18n.h>
 #include "meme-application.h"
 #include "adwaita.h"
 #include "meme-window.h"
 #include "meme-gpu.h"
+#include "config.h"
 
 struct _MemeApplication
 {
@@ -66,10 +66,44 @@ meme_application_about_action (GSimpleAction *action,
                                 GVariant      *parameter,
                                 gpointer       user_data)
 {
-  MemeApplication *self = user_data;
-  GtkWindow *window = gtk_application_get_active_window (GTK_APPLICATION (self));
-
-  show_about_dialog (window);
+    MemeApplication *self = user_data;
+    GtkWindow *window = gtk_application_get_active_window (GTK_APPLICATION (self));
+    
+    g_autofree char *os_release_content = NULL;
+    g_autoptr(GError) error = NULL;
+    
+    if (!g_file_get_contents("/etc/os-release", &os_release_content, NULL, &error)) {
+        os_release_content = g_strdup("Could not read /etc/os-release");
+        g_clear_error(&error);
+    }
+        
+    g_autofree char *debug_text = g_strdup_printf (
+        "Memerist %s\n"
+        "GTK version: %d.%d.%d\n"
+        "--- Runtime info ---\n"
+        "%s",
+        PACKAGE_VERSION,
+        gtk_get_major_version (),
+        gtk_get_minor_version (),
+        gtk_get_micro_version (),
+        os_release_content
+    );
+    
+    AdwDialog *dialog = adw_about_dialog_new_from_appdata(
+        "/io/github/vani_tty1/memerist/io.github.vani_tty1.memerist.metainfo.xml", 
+        PACKAGE_VERSION
+    );
+    
+    static const char *developers[] = {"vani-tty1", NULL};
+    static const char *designers[] = {"vani-tty1", NULL};
+    
+    adw_about_dialog_set_developers(ADW_ABOUT_DIALOG(dialog), developers);
+    adw_about_dialog_set_designers(ADW_ABOUT_DIALOG(dialog), designers);
+    adw_about_dialog_set_version(ADW_ABOUT_DIALOG(dialog), PACKAGE_VERSION);
+    adw_about_dialog_set_debug_info(ADW_ABOUT_DIALOG(dialog), debug_text);
+    adw_about_dialog_set_debug_info_filename(ADW_ABOUT_DIALOG(dialog), "meme-debug.txt");
+    
+    adw_dialog_present(dialog, GTK_WIDGET(window));
 }
 
 
